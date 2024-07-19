84b095095deef238281c9a0af1e6cba2cb615f9e
Ilia Baev <baevilia0809@gmail.com>
2024-07-19 14:44:52 +0300

    В решение добавлена библиотека классов с тестами. Установлены необходимые пакеты.

--- NoizeRoomApp/Controllers/BookingController.cs
+++ NoizeRoomApp/Controllers/BookingController.cs
@@ -10,35 +10,35 @@ namespace NoizeRoomApp.Controllers
     [Route("api/[controller]")]
     public class BookingController : ControllerBase
     {
-        List<Booking> bookings = new()
-        {
-            new Booking {Id = Guid.NewGuid(), BookerName="Петя", Date = new DateTime(2024, 7, 3), TimeFrom = DateTime.Now, TimeTo = DateTime.Now.AddHours(2) },
-            new Booking {Id = Guid.NewGuid(), BookerName="Вася", Date = new DateTime(2024, 7, 1), TimeFrom = DateTime.Now, TimeTo = DateTime.Now.AddHours(2) },
-            new Booking {Id = Guid.NewGuid(), BookerName="Дима", Date = new DateTime(2024, 7, 1), TimeFrom = DateTime.Now, TimeTo = DateTime.Now.AddHours(2) },
-            new Booking {Id = Guid.NewGuid(), BookerName="Вовчик", Date = new DateTime(2024, 7, 3), TimeFrom = DateTime.Now, TimeTo = DateTime.Now.AddHours(2), BookerId = Guid.Parse("fe442bb5-514a-4c27-9b26-f6219866035b") }
-        };
+    
 
         private readonly PostgreSQLContext _context;
         public BookingController(PostgreSQLContext context) 
         {
             _context = context;
         }
-        List<User> testUsers = new()
-        {
-            new User { Id = Guid.Parse("fe442bb5-514a-4c27-9b26-f6219866035b"), Name = "Вовчик", RoleId =2}
-        };
-
+       
+        /// <summary>
+        /// Метод получения броней на определённый промежуток данных. На вход принимает дату "От" и дату "До"
+        /// </summary>
+        /// <param name="request">Возвращается коллекцию вида <DateTime date, bool isBooking></param>
+        /// <returns></returns>
         [HttpGet("getBookingsByDate")]
         public async Task<IActionResult> GetBookingByDate([FromBody] GetBookingByDateRequest request)
         {
+            //Генерируется список дат за период
             List<DateTime> dates = DatesGeneration(request.dateFrom, request.dateTo);
+
             List<GetBookingByDateResponse> responce = new();
+        
             foreach (var date in dates) 
             {
+                //Если на данную дату присутствует хоть одна запись, isBooking = true
                 if (_context.Bookings.Any(b => b.Date.Equals(date)))
                 {
                     responce.Add(new GetBookingByDateResponse(date, true));
                 }
+                //Если нет, isBooking = false
                 else 
                 {
                     responce.Add(new GetBookingByDateResponse(date, false));
@@ -49,26 +49,38 @@ namespace NoizeRoomApp.Controllers
             return Ok(responce);
         }
 
-       
+        /// <summary>
+        /// Запрос на получение статистики по посещениям за месяц. На вход принимает дату из которой выбирается месяц
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Возвращает коллекцию вида <DateTime date, int count></returns>
         [HttpGet("getStatistic")]
          public async Task<IActionResult> GetStatisticByMonth([FromBody] GetStatisticRequest request)
         {
+            //Генерация дат месяца
             List<DateTime> dates = GenerateDatesByMonth(request.date);
+            //Генерация статистики
             List<GetStatisticResponse> responce = GenerateStatistic(dates);
 
             return Ok(responce);
         }
 
-        
 
 
 
+        /// <summary>
+        /// Запрос на создание брони. 
+        /// </summary>
+        /// <param name="request">Принимает идентификатор пользователя, дату бронирования, время "От" и время "До"</param>
+        /// <returns></returns>
         [HttpPost("book")]
         public async Task<IActionResult> Book([FromBody] AddBookRequest request)
         {
+            //Поиск имени бронирующего по его идентификатору
             string bookerName = _context.Users.Where(u => u.Id.Equals(Guid.Parse(request.bookerId))).Select(u=>u.Name).FirstOrDefault();
             try
             {
+                //Создание брони
                 BookingEntity newBook = new()
                 {
                     Id = Guid.NewGuid(),
@@ -83,7 +95,7 @@ namespace NoizeRoomApp.Controllers
                 {
                     return NoContent();
                 }
-
+                //Добавление и сохранение брони
                 _context.Bookings.Add(newBook);
                 _context.SaveChanges();
                 return Ok();
@@ -96,27 +108,40 @@ namespace NoizeRoomApp.Controllers
 
 
         }
-
+        /// <summary>
+        /// Запрос на удаление брони. 
+        /// </summary>
+        /// <param name="request">Принимает идентификатор брони</param>
+        /// <returns></returns>
         [HttpDelete("deleteBook")]
         public async Task<IActionResult> BookDelete([FromBody] DeleteBookRequest request)
         {
+            //Поиск брони на удаление
             BookingEntity bookForDelete = _context.Bookings.Where(b => b.Id.Equals(Guid.Parse(request.id))).FirstOrDefault();
 
             if (bookForDelete is null) 
             {
                 return NoContent();
             }
+            //Удаление и сохранение изменений
             _context.Bookings.Remove(bookForDelete);
             _context.SaveChanges();
             return Ok();
         }
 
+        /// <summary>
+        /// Запрос на получение броней на день. 
+        /// </summary>
+        /// <param name="request">Принимает дату и идентификатор текущего пользователя</param>
+        /// <returns>Коллекция объектов вида <DateTime date, DateTime timeFrom, DateTime timeTo, string bookerName></returns>
         [HttpGet("getDayBooking")]
         public async Task<IActionResult> GetDayBooking([FromBody] GetBooksByDayRequest request)
         {
+            //Поиск текущего пользователя
             UserEntity currentUser = _context.Users.Where(u=>u.Id.Equals(Guid.Parse(request.userId))).FirstOrDefault();
 
             List<GetBooksByDayResponse> responce = new();
+            //Если роль текущего пользователя "Администратор", то выводится информация по всем броням на день
             if (currentUser.RoleId == 1)
             {
                 responce = (from books in _context.Bookings
@@ -125,6 +150,7 @@ namespace NoizeRoomApp.Controllers
 
                 return Ok(responce);
             }
+            //Если роль текущего пользователя "Пользователь", то выводится информация по его броням на день
             else if (currentUser.RoleId == 2)
             {
                 responce = (from books in _context.Bookings
@@ -138,16 +164,22 @@ namespace NoizeRoomApp.Controllers
             }
         }
 
+        /// <summary>
+        /// Запрос на редактирование брони. На вход принимает идентификатор пользователя, дату бронирования, время "От" и время "До"
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPut("bookUpdate")]
         public async Task<IActionResult> Update([FromBody] UpdateBookRequest request)
         {
+            //Поиск брони по идентификатору
             BookingEntity bookForUpdate = _context.Bookings.Where(b=>b.Id.Equals(Guid.Parse(request.id))).FirstOrDefault();
 
             if (bookForUpdate is null) 
             {
                 return NoContent();
             }
-
+            //Запись в БД
             try
             {
                 bookForUpdate.Date = request.date;
@@ -165,13 +197,19 @@ namespace NoizeRoomApp.Controllers
             }
         }
    
-
+        /// <summary>
+        /// Метод генерации данных по интервалу "От" и "До"
+        /// </summary>
+        /// <param name="dateFrom">Дата "От"</param>
+        /// <param name="dateTo">Дата "До"</param>
+        /// <returns></returns>
         private static List<DateTime> DatesGeneration(DateTime dateFrom, DateTime dateTo)
         {
+
             List<DateTime> dates = new();
             DateTime startDate = dateFrom;
             DateTime nextDate = startDate.AddDays(1);
-            int count = 1;
+            //Генерация происходит путём добавления по одному дню к дате, пока очередная дата для добавления не будет позже чем дата "До"
             while (nextDate <= dateTo)
             {
                 dates.Add(startDate);
@@ -182,12 +220,20 @@ namespace NoizeRoomApp.Controllers
 
             return dates;
         }
+
+        /// <summary>
+        /// Генерация дат на определённый месяц
+        /// </summary>
+        /// <param name="date">Дата из которой метод берёт месяц и год</param>
+        /// <returns></returns>
         private static List<DateTime> GenerateDatesByMonth(DateTime date)
         {
             List<DateTime> dates = new();
 
+            //Через метод DaysInMonth получается количество дней этого месяца
             int count = DateTime.DaysInMonth(date.Year, date.Month);
 
+            //Заполнение списка дат
             for (int i = 1; i <= count; i++)
             {
                 dates.Add(new DateTime(date.Year, date.Month, i));
@@ -195,9 +241,16 @@ namespace NoizeRoomApp.Controllers
 
             return dates;
         }
+
+        /// <summary>
+        /// Метод генерации статистики посещений за месяц
+        /// </summary>
+        /// <param name="dates">Список сгенерированных дат за месяц</param>
+        /// <returns></returns>
         private List<GetStatisticResponse> GenerateStatistic(List<DateTime> dates)
         {
             List<GetStatisticResponse> responce = new();
+            //По каждой дате проверяется количество записей о брони
             foreach (var date in dates)
             {
                 responce.Add(new GetStatisticResponse(date, _context.Bookings.Where(b => b.Date.Equals(date)).Count()));
--- NoizeRoomApp/Controllers/UserController.cs
+++ NoizeRoomApp/Controllers/UserController.cs
@@ -9,17 +9,15 @@ using NoizeRoomApp.Contracts.UserContracts;
 
 namespace NoizeRoomApp.Contracts
 {
+    /// <summary>
+    /// Вся логика работы приложения, связаная с пользователем
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        List<User> users = new()
-        {
-            new User { Id = Guid.Parse("fe442bb5-514a-4c27-9b26-f6219866035b"), Name="Олег", Email="oleg@mail.ru", Password="123", PhoneNumber="88005553535"}
-        };
 
         private readonly PostgreSQLContext _context;
-
 
 
         public UserController(PostgreSQLContext context)
@@ -28,90 +26,118 @@ namespace NoizeRoomApp.Contracts
 
         }
 
+        /// <summary>
+        /// Метод аутентификации пользоваеля, на вход требует строковые почту и пароль
+        /// </summary>
+        /// <param name="request">Возвращает идентификатор пользователя и токен доступа</param>
+        /// <returns></returns>
         [HttpPost("auth")]
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
+            //Создание хэшированного пароля, сначала входящий пароль расшифровывается в стандартный вид(DecodingPass), затем хэшируется(EncodingPassword)
             string password = EncodindPassword(DecodingPassword(request.password));
 
+            //Поиск в БД пользователя по его почте и паролю
             UserEntity user = _context.Users
                 .Where(u => u.Email.Equals(request.email) && u.Password.Equals(password))
                 .FirstOrDefault();
-                      
+             
+            //Если не пользователь не найден, возвращается ошибка 401 
             if (user is null)
             {
                 return Unauthorized();
             }
+            //Генерируется токен доступа
             user.AccessToken = Guid.NewGuid();
+
+            //Сохранить изменения
             _context.SaveChanges();
+
+            
             return Ok(new LoginResponse(user.Id.ToString(), user.AccessToken.ToString()));
         }
 
+        /// <summary>
+        /// Метод регистрации, на вход требует идентификатор, имя, почту, телефон, пароль, идентификатор типа уведомления и идентификатор роли
+        /// </summary>
+        /// <param name="registerRequest">Возвращает иденти</param>
+        /// <returns></returns>
         [HttpPost("reg")]
         public async Task<IActionResult> Registration([FromBody] RegistrationRequest registerRequest)
         {
 
-            var passFromReq = Convert.FromBase64String(registerRequest.password);
-            string encodedPassword = Encoding.UTF8.GetString(passFromReq);
+           //дешифровка полученного пароля
+            string password = DecodingPassword(registerRequest.password);
 
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < encodedPassword.Length; i += 2)
-            {
-                builder.Append(encodedPassword[i]);
-            };
-
-
-
+            //Создание нового пользователя
             UserEntity newUser = new()
             {
                 Id = Guid.NewGuid(),
                 Name = registerRequest.name,
                 Email = registerRequest.email,
                 PhoneNumber = registerRequest.phone,
-                Password = builder.ToString(),
-                AccessToken = Guid.NewGuid(),
+                Password = EncodindPassword(password),
                 NotifyTypeId = registerRequest.notifyTypeId,
                 RoleId = registerRequest.roleId
             };
 
             try
             {
+                //Добавление пользователя в БД
                 _context.Users.Add(newUser);
                 _context.SaveChanges();
                 return Ok(new RegistrationResponse(newUser.Id.ToString()));
 
             }
+            //При ошибке сохранения отправляется сообщение об ошибке
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
 
         }
-
+        /// <summary>
+        /// Запрос о получении пользователя, на вход принимает идентификатор пользователя
+        /// </summary>
+        /// <param name="request">Возвращает идентификатор, почту, пароль, номер телефона, тип уведомления и идентификатор роли</param>
+        /// <returns></returns>
         [HttpGet("getProfile")]
         public async Task<IActionResult> GetUserById([FromBody] GetProfileRequest request)
         {
 
-
+            //Поиск пользователя по идентификатору
             UserEntity user = _context.Users.Where(u => u.Id.Equals(Guid.Parse(request.id))).FirstOrDefault();
+
+            //Если пльзователь не найден, возвращается ошибка 201
             if (user is null)
             {
                 return NoContent();
             }
-            string notifyType = _context.Notifies.Find(user.NotifyTypeId).ToString();
+            //По идентификатору находится тип уведомления
+            string notifyType = _context.Notifies.Find(user.NotifyTypeId).Name;
             return Ok(new GetProfileResponse(user.Id, user.Name, user.Email, user.PhoneNumber, notifyType, user.RoleId));
         }
 
+
+        /// <summary>
+        /// Запрос на обновление профиля, на вход принимает имя, почту, номер телефона и тип уведомления
+        /// </summary>
+        /// <param name="request">Возвращает обновлённого пользователя</param>
+        /// <returns></returns>
         [HttpPut("updateProfile")]
         public async Task<IActionResult> UpdateUserInfo([FromBody] UpdateProfileRequest request)
         {
+            //Поиск пользователя по идентификатору
             UserEntity userForSave = _context.Users.Where(u => u.Id.Equals(Guid.Parse(request.id))).FirstOrDefault();
 
+            //Если не найден, ошибка 201
             if (userForSave is null)
             {
                 return NoContent();
             }
             try
             {
+                //Запись в базу данных новой информации
                 userForSave.Name = request.name;
                 userForSave.Email = request.email;
                 userForSave.PhoneNumber = request.phoneNumber;
@@ -128,19 +154,27 @@ namespace NoizeRoomApp.Contracts
             }
         }
 
+        /// <summary>
+        /// Запрос на смену пароля, на вход пароль и идентификатор пользователя
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPut("changePassword")]
         public async Task<IActionResult> ChangePassword([FromBody] ChangePasswRequest request)
         {
+            //Поиск пользователя
             UserEntity userForPassChange = _context.Users.Where(u => u.Id.Equals(Guid.Parse(request.id))).FirstOrDefault();
 
+            //Если не найден, ошибка 201
             if (userForPassChange is null)
             {
                 return NoContent();
             }
 
+            //Расшифровка полученного пароля
             string password =DecodingPassword(request.password);
 
-
+            //Хэширование пароля и запись в БД
             userForPassChange.Password =EncodindPassword(password);
 
             _context.SaveChanges();
@@ -148,12 +182,18 @@ namespace NoizeRoomApp.Contracts
             return Ok();
         }
 
-
+        /// <summary>
+        /// Метод дешифровки пароля, полученного в запросе. Пароль шифруется путём добавления после каждого символа пароля случайного символа(123 - 1a2b3c)
+        /// </summary>
+        /// <param name="cryptedPassword">Возвращает пароль в чистом виде</param>
+        /// <returns></returns>
         public string DecodingPassword(string cryptedPassword)
         {
+            //Конвертирование Base64 в string
             var encodedBytes = Convert.FromBase64String(cryptedPassword);
             string encodedPassword = Encoding.UTF8.GetString(encodedBytes);
 
+            //Сборка нового пароля StringBuilder
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < encodedPassword.Length; i += 2)
             {
@@ -162,7 +202,11 @@ namespace NoizeRoomApp.Contracts
             return builder.ToString();
 
         }
-
+        /// <summary>
+        /// Метод хэширования пароля хэшем MD5
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public string EncodindPassword(string password)
         {
 
